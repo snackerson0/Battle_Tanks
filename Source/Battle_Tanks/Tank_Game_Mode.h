76e9e46ac690282656fb57d5578cc139021032f9
Alex Raymondo <snackerson0@hotmail.com>
2018-06-03 08:13:02 -0400

    testing

---
+++ Source/Battle_Tanks/Tank_Game_Mode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Tank_Game_Mode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLE_TANKS_API ATank_Game_Mode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
